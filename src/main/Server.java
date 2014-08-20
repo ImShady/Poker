d7e942a9a9115d62241271fc12207a90eeee83c9
Mentalist14 <admin@shadycraft.net>
2014-08-20 00:53:46 -0400

    Setup basic server concepts including sockets and streams.

--- src/main/Server.java
+++ src/main/Server.java
@@ -6,10 +6,85 @@
 
 package main;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- *
  * @author Shady, Sam
- */
-public class Server {
+ **/
+public class Server 
+{
+    ObjectOutputStream output;
+    ObjectInputStream input;
+    ServerSocket server;
+    Socket connection;
+
+    //set up and run server
+    public void startRunning()
+    {
+        try 
+        {
+            server = new ServerSocket(4444, 6);
+
+            while (true)
+            {
+                try 
+                {
+                    waitForConnection();
+                    setupStreams();
+                    /* Note to Sam: You may want to not setup the streams until you've verified that there's an open spot on the table.
+                    Not sure yet, we'll figure it out soon*/
+                    
+                    //Sam's awesome algorithim to deal with client interactions is here.
+                } 
+                catch (EOFException e) 
+                {
+                    System.out.println("\n Server ended connection.");                    
+                } 
+                finally
+                {
+                    closeStreams();
+                }
+            }
+        }
+        catch (IOException e) 
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void waitForConnection() throws IOException
+    {
+        System.out.println("Waiting for someone to connect...\n");
+    }
     
+    private void setupStreams() throws IOException
+    {
+        output = new ObjectOutputStream(connection.getOutputStream());
+        input = new ObjectInputStream(connection.getInputStream());
+        output.flush();
+        System.out.println("Streams setup.");
+    }
+    
+    private void closeStreams()
+    {
+        System.out.println("Closing streams...");
+        try
+        {
+            output.close();
+            input.close();
+            connection.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Closing Streams failed: " + e.getMessage());
+        }
+    }
+    
+    
+
 }
